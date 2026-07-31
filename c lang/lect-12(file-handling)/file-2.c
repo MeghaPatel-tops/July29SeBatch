910d6cdfecc7aac1fc13ebfe147d9d44d7482982
megha <meghapatel1.tops@gmail.com>
2026-07-31 14:25:08 +0530

    file-andling

---
+++ c lang/lect-12(file-handling)/file-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	FILE *fp;
+	char ch;
+	fp=fopen("hello1.txt","w");
+    fputc('Y',fp);
+	fclose(fp);
+	
+	fp=fopen("hello1.txt","r");
+	ch=getc(fp);
+	printf("\n data from file=%c",ch);
+	fclose(fp);
+	
+}
